3f3598290401248eac220318a36aa5bba2508f3b
Nils Schweinsberg <mail@n-sch.de>
2010-03-05 13:01:39 +0100

    Changed the behaviour of EventLoop
        - timeoutAdd and inputAdd now have to call `prepare' before the can
          add their key. Timeout callbacks should get terminated when they
          return False, but this is only possible if they know their key
          before they get added to the list
        - Added exception handling in the events

--- src/Network/HsPurple/EventLoop.hs
+++ src/Network/HsPurple/EventLoop.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE ForeignFunctionInterface, ScopedTypeVariables #-}
 
 module Network.HsPurple.EventLoop
     (
@@ -32,7 +32,10 @@ module Network.HsPurple.EventLoop
     ) where
 
 import Control.Applicative
+import Control.Monad
 import Control.Concurrent.MVar
+
+import qualified Control.Exception as E
 
 import Foreign
 import Foreign.C
@@ -45,7 +48,7 @@ import qualified Data.Set as S
 
 data IFd = IFd
     { evId  :: EventId
-    , key   :: Either FdKey TimeoutKey
+    , key   :: Maybe (Either FdKey TimeoutKey)
     }
 
 instance Eq IFd where
@@ -76,24 +79,39 @@ foreign import ccall "purple_eventloop_get_ui_ops"
 -- Helper functions
 --------------------------------------------------------------------------------
 
-incHandles :: MVar (S.Set IFd) -> Either FdKey TimeoutKey -> IO Int
-incHandles m key' = modifyMVar m $ \s ->
-    let newH n = (S.insert (IFd n key') s, n)
-    in  return . newH $ if S.null s
-                          then 0
-                          else evId (S.findMax s) + 1
+-- | Generate a new EventId and return a function to add a Fd/TimeoutKey
+prepare :: MVar (S.Set IFd) -> IO (Either FdKey TimeoutKey -> IO (), EventId)
+prepare m = do
 
-lookupFdk :: FdKey -> S.Set IFd -> Maybe Int
+    eid <- modifyMVar m $ \s ->
+        let i = if S.null s then 0 else evId (S.findMax s) + 1
+        in return (S.insert (IFd i Nothing) s, i)
+
+    let f k = modifyMVar_ m $ \s -> return $ S.insert (IFd eid (Just k)) s
+    return (f, eid)
+
+-- | Find the EventId of a FdKey
+lookupFdk :: FdKey -> S.Set IFd -> Maybe EventId
 lookupFdk fdk s =
-    case S.elems $ S.filter ((Left fdk ==) . key) s of
+    case S.elems $ S.filter ((Just (Left fdk) ==) . key) s of
          [IFd i _] -> Just i
          _         -> Nothing
 
+-- | Find the Key of an Event
 lookupEventId :: EventId -> S.Set IFd -> Maybe (Either FdKey TimeoutKey)
 lookupEventId i s =
     case S.elems $ S.filter ((i ==) . evId) s of
-         [IFd _ ifd] -> Just ifd
-         _           -> Nothing
+         [IFd _ (Just ifd)] -> Just ifd
+         _                  -> Nothing
+
+-- | Handle IO exceptions
+safe :: IO () -> IO ()
+safe = safe' ()
+
+-- | Handle IO exceptions with a default value
+safe' :: a -> IO a -> IO a
+safe' a = E.handle (\(e :: E.IOException) -> do putStrLn $ "Network.HsPurple.EventLoop.safe: Exception caught: `" ++ show e ++ "'"
+                                                return a)
 
 
 
@@ -106,29 +124,33 @@ lookupEventId i s =
 inputAdd :: Events -> Fd -> Event -> EventInputFunc -> UserData -> IO EventId
 inputAdd evs fd event func ud = do
 
-    -- putStrLn $ "HsPurple.EventLoop.inputAdd - Fd: " ++ show fd
+    -- putStr   $ "Network.HsPurple.EventLoop.inputAdd - Fd: " ++ show fd
 
-    registerFd (eventManager evs) callback fd event >>= incHandles (handles evs) . Left
+    (f, i) <- prepare $ handles evs
+    registerFd (eventManager evs) callback fd event >>= f . Left
+
+    -- putStrLn " OK"
+    return i
 
   where callback :: IOCallback
         callback fdk ev = do
+            -- putStrLn $ "Network.HsPurple.EventLoop.inputAdd - Callback: " ++ show fdk ++ " - " ++ show ev
             i <- lookupFdk fdk `fmap` readMVar (handles evs)
             case i of
-                 Just i' -> func ud i' ev
-                 _       -> return ()
+                 Just i' -> safe $ func ud i' ev
+                 _       -> return () -- putStrLn "Event triggered but no callback found!"
 
 
 -- | Remove a input handler
 inputRemove :: Events -> EventId -> IO Bool
 inputRemove ae h = do
 
-    -- putStr $ "HsPurple.EventLoop.inputRemove - Event: " ++ show h
+    -- putStrLn $ "Network.HsPurple.EventLoop.inputRemove - Event: `" ++ show h ++ "'"
     fdk <- lookupEventId h `fmap` readMVar (handles ae)
     case fdk of
          Just (Left f) -> do unregisterFd (eventManager ae) f
-                             -- putStrLn " OK"
                              return True
-         _             -> do -- putStrLn " Fail"
+         _             -> do -- putStrLn $ "Network.HsPurple.EventLoop.inputRemove: No waiting event with id `" ++ show h ++ "'"
                              return False
 
 -- | Get the current error status for an input.
@@ -139,12 +161,17 @@ inputGetError _ _ _ = return $ Errno 0
 timeoutAdd :: Events -> Int -> EventGSourceFunc -> UserData -> IO EventId
 timeoutAdd ae interval func ud = do
 
-    -- putStrLn $ "HsPurple.EventLoop.timeoutAdd - Interval: " ++ show interval
+    -- putStrLn $ "Network.HsPurple.EventLoop.timeoutAdd - Interval: " ++ show interval
 
-    registerTimeout (eventManager ae) interval callback >>= incHandles (handles ae) . Right
+    (f,i) <- prepare $ handles ae
+    safe $ registerTimeout (eventManager ae) interval (callback i) >>= f . Right
+    return i
 
-  where callback :: TimeoutCallback -- type TimeoutCallback = IO ()
-        callback = () <$ func ud -- hmhmhm no this actually returns a Bool
+  where callback :: EventId -> TimeoutCallback -- type TimeoutCallback = IO ()
+        callback i = do
+            -- putStrLn $ "Network.HsPurple.EventLoop.timeoutAdd - Callback: `" ++ show i ++ "'"
+            b <- safe' False (func ud)
+            unless b $ () <$ timeoutRemove ae i
 
 -- | Creates a callback timer.
 timeoutAddSeconds :: Events -> Int -> EventGSourceFunc -> UserData -> IO EventId
@@ -157,13 +184,12 @@ timeoutAddSeconds ae interval func ud =
 timeoutRemove :: Events -> EventId -> IO Bool
 timeoutRemove ae h = do
 
-    -- putStr $ "HsPurple.EventLoop.timeoutRemove - Event: " ++ show h
+    -- putStrLn $ "Network.HsPurple.EventLoop.timeoutRemove - Event: " ++ show h
     k <- lookupEventId h `fmap` readMVar (handles ae)
     case k of
          Just (Right tk) -> do unregisterTimeout (eventManager ae) tk
-                               -- putStrLn " OK"
                                return True
-         _               -> do -- putStrLn " Fail"
+         _               -> do return () -- putStrLn $ "Network.HsPurple.EventLoop.timeoutRemove - Fail: No function found with EventId `" ++ show h ++ "'"
                                return False
 
 
